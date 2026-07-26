073c7127f7f893724355767ddec13de23929dccb
bhushan1042 <bhushanjadhav668@gmail.com>
2026-07-26 23:34:30 +0530

    fixes

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,18 +1,13 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://localhost:27017/bhidu', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(()=> console.log('connected to database'))
-.catch((err)=> console.log('custom error = '+ err));
-
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+{
     name: String,
     email: String,
     password: String
-}, { timestamps: true });
+},
+{
+    timestamps: true
+});
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
-
+module.exports = mongoose.model('User', userSchema);
